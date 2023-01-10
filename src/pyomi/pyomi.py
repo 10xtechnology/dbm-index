397102664ebdb9079ae65fdb02788afa2719a810
James <james.rao@ensoconnect.com>
2023-01-10 15:07:44 -0500

    refactor

--- src/pyomi/pyomi.py
+++ src/pyomi/pyomi.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from json import dumps, loads
-from typing import Optional, Union, Dict, List
+from typing import Optional, Union, Dict, List, Literal
 
 from .helpers import custom_hash
 
@@ -29,71 +29,80 @@ class Pyomi:
         self.key_delim = key_delim
 
     def create(self, resource: JsonDict) -> str:
-        resource_head_encoded = self.db.get('head', b'-1')
-        resource_head = int(resource_head_encoded.decode())
-        resource_id = str(resource_head + 1)
+        head_encoded = self.db.get('head', b'-1')
+        head = int(head_encoded.decode())
+        new_head = str(head + 1)
 
-        if resource_head >= 0:
-            self.db[self.key_delim.join([resource_id, 'next'])] = resource_head_encoded
+        if head >= 0:
+            self.db[self.key_delim.join([new_head, 'next'])] = head_encoded
 
-        self.db[self.key_delim.join([resource_id, 'head'])] = str(len(resource) - 1).encode()
+        self.db[self.key_delim.join([new_head, 'head'])] = str(len(resource) - 1).encode()
 
-        resource_id_encoded = resource_id.encode()
-        self.db['head'] = resource_id_encoded
+        new_head_encoded = new_head.encode()
+        self.db['head'] = new_head_encoded
 
         for index, (key, value) in enumerate(resource.items()):
-            key_id = str(index)
-            self.db[self.key_delim.join([resource_id, key_id, 'value'])] = key.encode()
+            resource_key_head = str(index)
+            self.db[self.key_delim.join([new_head, resource_key_head, 'value'])] = key.encode()
 
             if index > 0:
-                self.db[self.key_delim.join([resource_id, key_id, 'next'])] = str(index - 1).encode()
+                self.db[self.key_delim.join([new_head, resource_key_head, 'next'])] = str(index - 1).encode()
             
             value_dump = dumps(value)
-            key_hash = custom_hash(key)
-            value_hash = custom_hash(value_dump)
             encoded_value_dump = value_dump.encode()
 
-            self.db[self.key_delim.join([resource_id, key_hash])] = encoded_value_dump
+            key_hash = custom_hash(key)
+            value_hash = custom_hash(value_dump)
 
-            value_index_head_key = self.key_delim.join([key_hash, value_hash, 'head'])
-            value_index_head_encoded = self.db.get(value_index_head_key, b'-1')
-            value_index_head = int(value_index_head_encoded.decode())
+            self.db[self.key_delim.join([new_head, key_hash])] = encoded_value_dump
 
-            value_index_id = str(value_index_head + 1)
-            self.db[self.key_delim.join([key_hash, value_hash, value_index_id])] = resource_id_encoded
+            key_value_head_key = self.key_delim.join([key_hash, value_hash, 'head'])
+            key_value_head_encoded = self.db.get(key_value_head_key, b'-1')
+            key_value_head = int(key_value_head_encoded.decode())
 
-            if value_index_head >= 0:
-                self.db[self.key_delim.join([key_hash, value_hash, 'next'])] = value_index_head_encoded
+            key_value_new_head = str(key_value_head + 1)
+            self.db[self.key_delim.join([key_hash, value_hash, key_value_new_head])] = new_head_encoded
 
-            self.db[value_index_head_key] = value_index_id.encode()
+            if key_value_head >= 0:
+                self.db[self.key_delim.join([key_hash, value_hash, 'next'])] = key_value_head_encoded
+
+            self.db[key_value_head_key] = key_value_new_head.encode()
 
             key_head_key = self.key_delim.join([key_hash, 'head'])
-            key_head = self.db.get(key_head_key)
+            key_head_encoded = self.db.get(key_head_key)
 
-            if not key_head:
+            if not key_head_encoded:
                 self.db[key_head_key] = encoded_value_dump
                 continue
 
-            leading_value = loads(key_head.decode())
-            lagging_value = None 
+            key_head = loads(key_head_encoded.decode())
 
-            while parse_comparable_json(leading_value) < parse_comparable_json(value):
-                lagging_value = leading_value
-                leading_value_encoded = self.db.get(self.key_delim.join([key_hash, value_hash, 'next']))
-                if not leading_value_encoded:
-                    break 
-                leading_value = loads(leading_value_encoded.decode())
+            comparable_value = parse_comparable_json(value)
 
-            if leading_value_encoded:
-                self.db[self.key_delim.join([key_hash, value_hash, 'next'])] = leading_value_encoded
-            
-            if lagging_value:
+            if comparable_value > parse_comparable_json(key_head):
+                self.db[key_head_key] = encoded_value_dump
+                self.db[self.key_delim.join([key_hash, value_hash, 'next'])] = key_head_encoded
+            else:
+                leading_value_encoded = key_head_encoded
+                while leading_value_encoded:
+                    leading_value = loads(leading_value_encoded.decode())
+
+                    if comparable_value > parse_comparable_json(leading_value):
+                        break
+
+                    lagging_value = leading_value
+                    leading_value_encoded = self.db.get(self.key_delim.join([key_hash, custom_hash(leading_value), 'next']))
+                    leading_value = loads(leading_value_encoded.decode())
+
+                if leading_value_encoded:
+                    self.db[self.key_delim.join([key_hash, value_hash, 'next'])] = leading_value_encoded
+                
                 self.db[self.key_delim.join([key_hash, custom_hash(dumps(lagging_value)), 'next'])] = encoded_value_dump
 
         
-        return resource_id
+        return new_head
 
-    def retrieve(self, filters: JsonDict = {}, keys: Optional[List[str]] = None, skip: int = 0, take: int = 10, sort_key: Optional[str] = None, sort_dir_asc: bool = True):
+    def retrieve(self, filters: JsonDict = {}, keys: Optional[List[str]] = None, offset: int = 0, limit: int = 10, sort_key: Optional[str] = None, sort_direction: Literal['asc', 'desc'] = 'asc'):
         if not filters:
             head = self.db.get('head')
 
